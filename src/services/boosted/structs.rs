97506955f5466cd963e32def8da0a7ef467235ea
Dustin Rouillard <code@dstn.to>
2024-10-19 17:55:20 -0600

    Adding support for new boosted stat types

--- src/services/boosted/structs.rs
+++ src/services/boosted/structs.rs
@@ -16,15 +16,18 @@ pub struct RideStats {
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Stats {
-  pub boards: Boards,
+  pub boards: Vec<Board>,
   pub rides: ValueEntry,
   pub duration: ValueEntry,
   pub distance: ValueEntry,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Boards {
-  pub distance: f64,
+pub struct Board {
+  pub id: i64,
+  pub name: String,
+  pub odometer: f64,
+  pub battery: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
